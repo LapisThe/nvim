b8bf1676034c9ecfee76b8b85e10307ce9de8777
Lapis! <inskepticskeptic@gmail.com>
2025-03-01 01:54:36 -0800

    Add Neovide support

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.g.neovide_input_use_logo = 1
+vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,4 +8,12 @@ vim.opt.autoindent = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 
+vim.opt.guifont = "JetBrainsMono Nerd Font:h13"
+vim.opt.guicursor = "i:ver25-blinkwait100-blinkon500-blinkoff500"
+
 vim.lsp.set_log_level("off")
+
+if vim.g.neovide then
+	vim.g.neovide_cursor_animation_length = 0
+	vim.g.neovide_scroll_animation_length = 0.2
+end
