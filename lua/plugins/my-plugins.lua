067df3b933263eeb378d5e58f48728138eac7fa3
Lapis! <inskepticskeptic@gmail.com>
2025-05-01 18:43:03 -0700

    Fix eslint on windows OS

--- lua/plugins/my-plugins.lua
+++ lua/plugins/my-plugins.lua
@@ -35,6 +35,15 @@ return {
 						"--header-insertion=never",
 					},
 				},
+				-- Necessary for eslint on windows
+				eslint = {
+					on_new_config = function(config, new_root_dir)
+						config.settings.workspaceFolder = {
+							uri = vim.uri_from_fname(new_root_dir),
+							name = vim.fn.fnamemodify(new_root_dir, ":t"),
+						}
+					end,
+				},
 			},
 		},
 	},
