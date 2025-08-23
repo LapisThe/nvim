424bb02294149a730002c55a301cc91e8e1b26cc
Lapis! <87749912+LapisThe@users.noreply.github.com>
2025-08-23 16:24:11 +0700

    Add mini.icons

--- lua/plugins/my-plugins.lua
+++ lua/plugins/my-plugins.lua
@@ -109,7 +109,6 @@ return {
 	},
 	{
 		"pmizio/typescript-tools.nvim",
-		enabled = false,
 		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 		opts = {
 			settings = {
@@ -117,6 +116,22 @@ return {
 				tsserver_file_preferences = {
 					includeCompletionsForModuleExports = true,
 				},
+			},
+		},
+	},
+	{
+		"echasnovski/mini.icons",
+		opts = {
+			file = {
+				[".eslintrc.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+				[".node-version"] = { glyph = "", hl = "MiniIconsGreen" },
+				[".prettierrc"] = { glyph = "", hl = "MiniIconsPurple" },
+				[".yarnrc.yml"] = { glyph = "", hl = "MiniIconsBlue" },
+				["eslint.config.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+				["package.json"] = { glyph = "", hl = "MiniIconsGreen" },
+				["tsconfig.json"] = { glyph = "", hl = "MiniIconsAzure" },
+				["tsconfig.build.json"] = { glyph = "", hl = "MiniIconsAzure" },
+				["yarn.lock"] = { glyph = "", hl = "MiniIconsBlue" },
 			},
 		},
 	},
