d3cc259ef0ebf627a781980df34a030a73faf7b2
Lapis! <87749912+LapisThe@users.noreply.github.com>
2026-01-18 22:58:40 -0800

    Change Neovide opacity

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,12 @@ vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true 
 vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("t", "<Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
+
+if vim.g.neovide then
+	vim.api.nvim_set_keymap("v", "<sc-c>", '"+y', { noremap = true })
+	vim.api.nvim_set_keymap("n", "<sc-v>", 'l"+P', { noremap = true })
+	vim.api.nvim_set_keymap("v", "<sc-v>", '"+P', { noremap = true })
+	vim.api.nvim_set_keymap("c", "<sc-v>", '<C-o>l<C-o>"+<C-o>P<C-o>l', { noremap = true })
+	vim.api.nvim_set_keymap("i", "<sc-v>", '<ESC>l"+Pli', { noremap = true })
+	vim.api.nvim_set_keymap("t", "<sc-v>", '<C-\\><C-n>"+Pi', { noremap = true })
+end
